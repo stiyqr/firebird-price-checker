a8d06602737701768f44507fca5ddedef9af1eae
stiyqr <stiyqr@gmail.com>
2023-06-21 22:10:54 +0800

    Update history-table.js to allow search on local table data

--- front-end/history-table.js
+++ front-end/history-table.js
@@ -1,23 +1,61 @@
-const table = [];
+const tableArr = []
 
-for (let i = 0; i < 10; i++) {
-    const tableEntry = { code: "product code" + (i + 1), price: "price" + (i + 1) };
-    table[table.length] = tableEntry;
-
+// function to append new data to tableArr
+function addDataToArr(addedProductCode) {
+    var addedPrice = "price " + (tableArr.length + 1);
+    tableArr.unshift([addedProductCode, addedPrice]);
 }
 
-function addMoreRows() {
-    var currentProductCode = document.getElementById('product_code').value;
-    var table = document.getElementById('price_table');
+// function to check is product code exists in tableArr
+function checkCurrentTable(addedProductCode) {
+    let matchingCodeIndex = -1;
+    let tempPrice = "";
+    for (let i = 0; i < tableArr.length; i++) {
+        if (addedProductCode == tableArr[i][0]) {
+            matchingCodeIndex = i;
+            tempPrice = tableArr[i][1];
+            break;
+        }
+    }
 
-
-
-    var row = table.insertRow(1);
-
-    var productCodeCol = row.insertCell(0);
-    var priceCol = row.insertCell(1);
-
-    productCodeCol.innerHTML = currentProductCode;
-    priceCol.innerHTML = "price";
-    //total.innerHTML = (price_col.innerHTML + points.innerHTML).toString();
+    if (matchingCodeIndex > -1) {
+        // move searched product code to top
+        tableArr.splice(matchingCodeIndex, 1);
+        tableArr.unshift([addedProductCode, tempPrice]);
+    }
+    else {
+        addDataToArr(addedProductCode);
+    }
 }
+
+
+//store table data element
+const tableDataElem = document.querySelector('#append_data');
+
+// function to build table
+function buildTable() {
+    // get product code input
+    var currentProductCode = document.getElementById('inp_product_code').value.trim();
+    if (currentProductCode != "") {
+        checkCurrentTable(currentProductCode);
+    }
+
+    // check if table data is empty
+    if (tableArr.length <= 0) {
+        return;
+    }
+
+    // reset table data
+    tableDataElem.innerHTML = "";
+
+    // build table according to newest data
+    for (let i = 0; i < tableArr.length; i++) {
+        tableDataElem.innerHTML += `
+                <tr>
+                    <td>${tableArr[i][0]}</td>
+                    <td>${tableArr[i][1]}</td>
+                </tr>
+            `;
+    }
+}
+buildTable();
